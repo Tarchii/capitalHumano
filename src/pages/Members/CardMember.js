c7129952d15bd9c5417961a0fa16666bb67e5ccd
ftarchini <ftarchini@totalcoin.com>
2022-11-02 21:37:51 -0300

    listo pagina de miembros con cards terminadas

--- src/pages/Members/CardMember.js
+++ src/pages/Members/CardMember.js
@@ -2,21 +2,75 @@ import { Card } from 'antd';
 import styled from 'styled-components';
 const { Meta } = Card;
 const CardMember = ({ name, legajo, email, img }) => (
-    <Card
-        hoverable
-        style={{
-            width: 240,
-            margin: 10,
-        }}
-        cover={<img alt="member image" src={img} />}
+    <CardContainer
     >
-        <Meta title={`[${legajo}] ${name}`} description={email} />
-    </Card>
+        <ImageRow>
+            <ImageWrapper>
+                <img src={img} alt="member image" />
+            </ImageWrapper>
+        </ImageRow>
+        <Name>
+            [{legajo}] {name}
+        </Name>
+        <Email>
+            {email}
+        </Email>
+    </CardContainer>
 );
 
-const Descriptions = styled.div`
+const CardContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr;
+grid-template-rows: repeat(5, 1fr);
+grid-column-gap: 0px;
+grid-row-gap: 0px;
+width: 250px;
+height: 300px;
+border-radius: 10px;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+padding: 15px;
+margin: 15px;
+`;
+
+const ImageRow = styled.div`
+grid-area: 1 / 1 / 4 / 2;
 display: flex;
-flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+
+const ImageWrapper = styled.div`
+width: 150px;
+height: 150px;
+border-radius: 50%;
+overflow: hidden;
+
+
+img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+`;
+
+const Name = styled.div`
+grid-area: 4 / 1 / 5 / 2;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 15px;
+font-weight: bolder;
+text-align: center;
+`;
+
+const Email = styled.div`
+grid-area: 5 / 1 / 6 / 2;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 10px;
+font-weight: bolder;
 `;
 
 export default CardMember;
--- src/pages/Members/index.js
+++ src/pages/Members/index.js
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 import AppLayout from '../../components/layout/AppLayout'
 import CardMember from './CardMember';
 import franco_tarchini from '../../assets/img/franco_tarchini.jpg';
+import guillermo_alonso from '../../assets/img/guillermo_alonso.jpeg';
+import faride_chair from '../../assets/img/faride_chair.jpeg';
+import ivan_yuste from '../../assets/img/ivan_yuste.jpeg';
+import josefina_japaze from '../../assets/img/josefina_japaze.jpeg';
+import juan_bustos_thames from '../../assets/img/juan_bustos_thames.jpeg';
+import luciana_olmos from '../../assets/img/luciana_olmos.jpeg';
 
 const Members = () => {
     return (
@@ -12,13 +18,13 @@ const Members = () => {
                     Integrantes
                 </PageTitle>
                 <CardsWrapper>
-                    <CardMember name="Franco Tarchini" legajo="45.189" email="francotarchini97@gmail.com" img={franco_tarchini} />
-                    <CardMember name="Franco Tarchini" legajo="45.189" email="francotarchini97@gmail.com" img={franco_tarchini} />
-                    <CardMember name="Franco Tarchini" legajo="45.189" email="francotarchini97@gmail.com" img={franco_tarchini} />
-                    <CardMember name="Franco Tarchini" legajo="45.189" email="francotarchini97@gmail.com" img={franco_tarchini} />
-                    <CardMember name="Franco Tarchini" legajo="45.189" email="francotarchini97@gmail.com" img={franco_tarchini} />
-                    <CardMember name="Franco Tarchini" legajo="45.189" email="francotarchini97@gmail.com" img={franco_tarchini} />
-                    <CardMember name="Franco Tarchini" legajo="45.189" email="francotarchini97@gmail.com" img={franco_tarchini} />
+                    <CardMember name="Alonso Torino, Guillermo" legajo="45.205" email="guillermo.alonsotorino@alu.frt.utn.edu.ar" img={guillermo_alonso} />
+                    <CardMember name="Arévalo, Luciana Olmos" legajo="44.686" email="lucianaarevalo@alu.frt.utn.edu.ar" img={luciana_olmos} />
+                    <CardMember name="Bustos Thames, Juan Pablo" legajo="46.323" email="juan.bustosthames@alu.frt.utn.edu.ar" img={juan_bustos_thames} />
+                    <CardMember name="Chair Hasay, Faride" legajo="44.480" email="Faride.ChairHasay@alu.frt.utn.edu.ar" img={faride_chair} />
+                    <CardMember name="Japaze, María Josefina" legajo="44.640" email="Maria.japaze@alu.utn.frt.edu.ar" img={josefina_japaze} />
+                    <CardMember name="Tarchini, Franco" legajo="45.189" email="franco.tarchini@alu.frt.utn.edu.ar" img={franco_tarchini} />
+                    <CardMember name="Yuste, Leandro Ivan" legajo="44.686" email="leandroyuste@alu.frt.utn.edu.ar" img={ivan_yuste} />
                 </CardsWrapper>
             </Container>
         </AppLayout>
